cae4bb4e19eb9581d9c7ba461e7cb006623a4153
Sidd Srinivasan <siddsrinivasan@sidds-mbp.lan>
2019-04-03 15:52:40 +0200

    credentials de firebase

--- codigo/Agenda/src/app/app.module.ts
+++ codigo/Agenda/src/app/app.module.ts
@@ -9,13 +9,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {FIREBASE_CONFIG} from '../app/firebase.credentials';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database'
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   providers: [
     StatusBar,
---
+++ codigo/Agenda/src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBRGiWtIPeAEPjcl9rziUfiNhoTsmlWtN4",
+    authDomain: "micro-practica-8.firebaseapp.com",
+    databaseURL: "https://micro-practica-8.firebaseio.com",
+    projectId: "micro-practica-8",
+    storageBucket: "micro-practica-8.appspot.com",
+    messagingSenderId: "761386424566"
+  };
